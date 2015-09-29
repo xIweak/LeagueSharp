641e5435f6d3f4d39f6e3dd55af2e63fc5af2215
xIweak <xIweak@users.noreply.github.com>
2015-09-29 09:02:42 -0600

    Update Program.cs

--- Viktor/Program.cs
+++ Viktor/Program.cs
@@ -1,7 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 using LeagueSharp;
 using LeagueSharp.Common;
+using SharpDX;
+
 using Color = System.Drawing.Color;
 
 namespace Viktor
